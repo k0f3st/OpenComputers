5057b8649f5ef1aad9b75c6589070e771fe68f2e
k0f3st <minecraft.gamemaster@yandex.ru>
2017-02-16 17:53:19 +0400

    Улучшение контроля разрешения

--- programs/drawimage.lua
+++ programs/drawimage.lua
@@ -1,4 +1,8 @@
 local args = {...}
 local image = require("image")
+local component = require("component")
+local gpu = component.gpu
+
+gpu.setResolution(160,50)
 local loadimage = image.load(args[1])
 image.draw(1, 1, loadimage)
